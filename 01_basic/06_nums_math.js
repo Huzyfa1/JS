04687084133140565f306a4c1218f1cf505dec78
Muhammad Huzaifa <115704991+Huzyfa1@users.noreply.github.com>
2024-08-09 20:03:30 +0000

    number and math

---
+++ 01_basic/06_nums_math.js
@@ -0,0 +1,37 @@
+const score = 400
+// console.log(score);
+
+
+const balance = new Number(100)
+// console.log(balance);
+
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+
+const otherNumber = 123.8923
+
+// console.log(otherNumber.toPrecision(4));
+
+const hunreds = 1000000
+// console.log(hunreds.toLocaleString('en-IN'));
+
+
+
+// ++++++++++++++++++++++ MATHS +++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.3));
+// console.log(Math.ceil(4.3));
+// console.log(Math.floor(4.8));
+
+// console.log(Math.random());
+// console.log(Math.floor((Math.random() * 10) + 1));
+
+
+const min = 1 
+const max = 6
+
+console.log(Math.floor(Math.random() * (max - min + 1) + min));
